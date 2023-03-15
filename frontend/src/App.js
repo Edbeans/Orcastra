d3cb05ee45048c9034c3937fa86c15985756bdaf
Daniel Liem <danielliem49@gmail.com>
2023-03-15 13:42:58 -0700

    ISP Styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import LoginSignUpPage from './components/Auth/LoginSignUpPage'
 import { getCurrentUser } from './store/session';
 import FeedPage from './components/Feed/FeedPage';
 import Sidebar from './components/Navigation/Sidebar';
-import CreateIdeaPage from './components/CreateIdeaPage';
+import CreateIdeaPage from './components/CreateIdeaPage/CreateIdeaPage';
 import IdeaShowPage from './components/IdeaShowPage/IdeaShowPage';
 import UserIdeas from './components/UserIdeas/UserIdeas';
 
--- frontend/src/components/IdeaShowPage/IdeaShowPage.jsx
+++ frontend/src/components/IdeaShowPage/IdeaShowPage.jsx
@@ -1,32 +1,64 @@
 import { fetchIdea, getIdea } from "../../store/idea"
 import { useParams } from "react-router-dom"
 import { useSelector, useDispatch } from "react-redux"
-import { useEffect } from "react"
+import { useState, useEffect } from "react"
+// import { createComment } from ...;
 import "./IdeaShowPage.css"
 
-export default function IdeaShowPage(){ 
+export default function IdeaShowPage() {
     const dispatch = useDispatch()
-    const {ideaId} = useParams()
+    const [comment, setComment] = useState('');
+    const { ideaId } = useParams()
     const idea = useSelector(getIdea(ideaId))
+
+    function handleCommentSubmit(e, errors) {
+        e.preventDefault()
+        const newComment = { comment }
+        if (errors && Object.values(errors).length === 0) {
+            return dispatch(createComment(newIdea))
+        }
+    }
 
     useEffect(() => {
         dispatch(fetchIdea(ideaId))
     }, [dispatch, ideaId])
     if (!idea) {
-        return null 
+        return null
     } else {
-    return ( 
-        <div className="isp-container">
-        
-            <div className="isp-header">
-                <div className="isp-title">{idea.title}</div>
-                {/* <div className="isp-description">{idea.body}</div> */}
-            </div>
+        return (
+            <div className="isp-container">
 
-            <div className="isp-media-container">
-                <div className="isp-image"></div>
+                <div className="isp-media-container">
+                    <div className="isp-image"></div>
+                </div>
+
+
+                <div className="isp-info-container">
+                    <div className="isp-title">{idea.title}</div>
+                    <div className="isp-description">{idea.body}</div>
+                    <div className="isp-actions">
+                        <button>Bid</button>
+                        <button>Save</button>
+                        <button>Contact</button>
+                    </div>
+                </div>
+
+                <div className="isp-comments-container">
+                    <form className="isp-create-comment-form" onSubmit={(e) => { handleCommentSubmit(e, errors)}} >
+                        <div className='form-input-group'>
+                            <textarea className="form-inputs" onChange={(e) => setComment(e.target.value)} required />
+                            <span className="form-input-labels">Write your comment here</span>
+                        </div>
+                        <button>Submit</button>
+                    </form>
+                    <div className="isp-comments-display">
+                        {/* {idea.comments.map((comment) => {
+                            <div>{comment.}</div>
+                        })} */}
+                    </div>
+                </div>
+
             </div>
-        
-        </div>
-    )}
+        )
+    }
 }
