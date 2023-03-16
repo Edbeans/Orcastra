80426fe64e190b5267d3729dccea0b6ad7c8ca22
Stephen Wong <stephenmdwong@gmail.com>
2023-03-16 13:50:19 -0700

    Add functionality to delete, add history for certain buttons

--- frontend/src/components/IdeaShowPage/IdeaShowPage.jsx
+++ frontend/src/components/IdeaShowPage/IdeaShowPage.jsx
@@ -1,51 +1,58 @@
-import { fetchIdea, getIdea } from '../../store/idea';
+import { deleteIdea, fetchIdea, getIdea } from '../../store/idea';
 import { useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { useState, useEffect } from 'react';
 import {
-  createComment,
-  fetchIdeaComments,
+    createComment,
+    fetchIdeaComments,
 } from '../../store/comment';
+import { useHistory } from 'react-router-dom';
 import './IdeaShowPage.css';
 import EditModalButton from '../IdeaEditModal';
 import { margin, width } from '@mui/system';
 import CommentContainer from '../Comments/CommentContainer';
 
 export default function IdeaShowPage() {
-  const dispatch = useDispatch();
-  // const [comment, setComment] = useState('');
-  const { ideaId } = useParams();
-  const idea = useSelector(getIdea(ideaId));
+    const dispatch = useDispatch();
+    const history = useHistory()
+    // const [comment, setComment] = useState('');
+    const { ideaId } = useParams();
+    const idea = useSelector(getIdea(ideaId));
 
-  useEffect(() => {
-    dispatch(fetchIdea(ideaId));
-  }, [dispatch, ideaId]);
+    useEffect(() => {
+        dispatch(fetchIdea(ideaId));
+    }, [dispatch, ideaId]);
 
-  if (!idea) {
-    return null;
-  } else {
-    return (
-      <div className='isp-container'>
-        <div className='isp-media-container'>
-          <div className='isp-image'></div>
-        </div>
+    function handleDelete() {
+        dispatch(deleteIdea(ideaId)).then(history.push('/'))
+    }
 
-        <div className='isp-info-container'>
-          <div className='isp-title'>{idea.title}</div>
-          <div className='isp-description'>{idea.body}</div>
-          <div className='isp-actions'>
-            <button>Bid</button>
-            <button>Save</button>
-            <button>Contact</button>
-          </div>
+    if (!idea) {
+        return null;
+    } else {
+        return (
+            <div className='isp-container'>
+                <div className='isp-media-container'>
+                    <div className='isp-image'></div>
+                </div>
 
-          <EditModalButton idea={idea} />
-        </div>
+                <div className='isp-info-container'>
+                    <div className='isp-title'>{idea.title}</div>
+                    <div className='isp-description'>{idea.body}</div>
+                    <div className='isp-actions'>
+                        <button>Bid</button>
+                        <button>Save</button>
+                        <button>Contact</button>
+                        <button onClick={handleDelete}>Delete</button>
+                    </div>
 
-        <div className='isp-comments-container'>
-          <CommentContainer idea={idea} />
-        </div>
-      </div>
-    );
-  }
+                    <EditModalButton idea={idea} />
+                </div>
+
+                <div className='isp-comments-container'>
+                    <CommentContainer idea={idea} />
+                </div>
+            </div>
+        );
+    }
 }
