1c921c2ada94588e08f8ceb4214a4709d6880a62
Edward Ying <edwardying98@gmail.com>
2023-03-16 23:22:47 -0700

    update user show page

--- frontend/src/components/UserShow/UserShow.jsx
+++ frontend/src/components/UserShow/UserShow.jsx
@@ -20,38 +20,6 @@ export default function UserIdeas() {
     
     return (
         <>
-            {/* <div className="usershow-container" data-aos="fade-up" data-aos-duration="1000">
-                <div className="usershow-content">
-
-                    <div className="usershow-info-header">
-                        <div className="usershow-profile-img">
-                        </div>
-                        <div className="usershow-username-container">
-                            <div className="usershow-username">
-                                {user.username}
-                            </div>
-                            <div className="usershow-email">
-                                {user.email}
-                            </div>
-                        </div>
-
-                        <div className="usershow-stats">
-                            <div className="usershow-num-ideas">{`Ideas: ${filteredIdeas.length}`}</div>
-                            <div className="usershow-num-comments">{`Comments Received: ${filteredIdeas.length}`}</div>
-                        </div>    
-                        
-                    </div>
-
-                    <div className="usershow-ideas">
-                        <div className="usi-header">
-                            <h1 className="usi-header-text">Your Ideas:</h1>
-                        </div>
-                        {console.log("FILTERED", filteredIdeas)}
-                        {filteredIdeas.map(idea => <UserIdeaIndexItem idea={idea} />)}
-                    </div>
-
-                </div>
-            </div> */}
             <div className='usp'>
 
                 <div className='usp-main'>
@@ -72,11 +40,11 @@ export default function UserIdeas() {
                         </main>
                         {/* USER BIO AND INFORMATION  */}
                         <div className='sp-left-container'>
-                            <div className='author-bio-container'>
-                                <div className='author-bio-inside-container'>
-                                    <div className='author-bio-content'>
-                                        <div className='abc-1'>
-                                            <div className='abc-2'>
+                            <div className='user-bio-container'>
+                                <div className='user-bio-inside-container'>
+                                    <div className='user-bio-content'>
+                                        <div className='ubc-1'>
+                                            <div className='ubc-2'>
                                                 <div className="usershow-profile-img" />
                                                 <div className='profile-name'>{user.username}</div>
                                                 <div className="usershow-stats">
