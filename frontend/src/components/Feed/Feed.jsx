66aa96c3362a40270021454f6df9e41aff092218
Daniel Liem <danielliem49@gmail.com>
2023-03-29 15:06:15 -0700

    initial feed changes

--- frontend/src/components/Feed/Feed.jsx
+++ frontend/src/components/Feed/Feed.jsx
@@ -1,17 +1,32 @@
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import { fetchIdeas, getIdeas } from "../../store/idea";
+import { useDispatch, useSelector } from 'react-redux';
+import { useState, useEffect } from 'react';
+import { fetchIdeas, getIdeas } from '../../store/idea';
+import IdeaIndexItem from './IdeaIndexItem';
+import './Feed.css'
+import Aos from 'aos';
 
-export default function Feed() {
-    const dispatch = useDispatch();
-    const ideas = useSelector(getIdeas);
+export default function FeedPage() {
+    const dispatch = useDispatch()
+    const ideas = useSelector(getIdeas)
 
     useEffect(() => {
         dispatch(fetchIdeas())
     }, [dispatch])
 
     return (
-        <>
-        </>
+        <div className='feed-container'>
+
+            <div className='featured-idea-container'>
+
+            </div>
+
+            <div className='feed-background'>
+                <div className='feed-page-wrapper'>
+                    <div className='feed-wrapper'>
+                        {ideas.slice(1).map((idea) => <IdeaIndexItem idea={idea} key={idea.id} />)}
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
