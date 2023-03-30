c7e4cf1902d83c27ed53f04b92d14020c978b701
Daniel Liem <danielliem49@gmail.com>
2023-03-30 12:33:15 -0700

    add feedindexitem animations

--- frontend/src/components/Feed/Feed.jsx
+++ frontend/src/components/Feed/Feed.jsx
@@ -5,6 +5,7 @@ import IdeaIndexItem from './IdeaIndexItem';
 import { Link } from 'react-router-dom';
 import './Feed.css'
 import Aos from 'aos';
+import 'aos/dist/aos.css';
 
 export default function FeedPage() {
     const dispatch = useDispatch()
@@ -24,14 +25,13 @@ export default function FeedPage() {
         }
     }, [ideas])
 
-
     return (
         <>
             {randomizedIdeas && randomizedIdeas.length !== 0 && (
                 <div className='feed-container'>
-
-                    <div className='feed-upper-container'>
-                        <div className='featured-idea-card'>
+                    
+                    <div className='feed-upper-container' data-aos="fade-up" data-aos-duration="1000">
+                        <div className='featured-idea-card' >
                             <div className='featured-idea-header'>Today's featured idea:</div>
                             <div className='featured-idea-properties'>
                                 <div className='featured-idea-info'>
@@ -45,24 +45,43 @@ export default function FeedPage() {
                                         <span>Read More</span>
                                     </Link>
                                 </div>
-
                                 <Link to={`/idea/${randomizedIdeas[0]._id}`} className='index-item-link'>
                                     <img className='featured-idea-img' src={randomizedIdeas[0].imageUrls[0]}></img>
                                 </Link>
                             </div>
                         </div>
-
                         <div className='feed-search-card'>
 
                         </div>
                     </div>
 
+                    <select className='feed-filter'>
+                        <option value="" disabled selected>Filter by:</option>
+                        <option value="option1">Most Bids</option>
+                        <option value="option2">Trending</option>
+                        <option value="option3">Newest</option>
+                        <option value="option4">Oldest</option>
+                    </select>
 
                     <div className='feed-background'>
                         <div className='feed-page-wrapper'>
                             <div className='feed-wrapper'>
 
-                                {randomizedIdeas.slice(1).map((idea) => <IdeaIndexItem idea={idea} key={idea.id} />)}
+                                {randomizedIdeas.slice(1).map((idea, index) => {
+                                    let ideaIndexOrder;
+                                    if (index % 4 === 0) {
+                                        ideaIndexOrder = 'index-item-order1';
+                                    } else if (index % 4 === 1) {
+                                        ideaIndexOrder = 'index-item-order2';
+                                    } else if (index % 4 === 2) {
+                                        ideaIndexOrder = 'index-item-order3';
+                                    } else {
+                                        ideaIndexOrder = 'index-item-order4';
+                                    }
+
+                                    return <IdeaIndexItem idea={idea} ideaIndexOrder={ideaIndexOrder} key={idea.id} />;
+                                })}
+
                             </div>
                         </div>
                     </div>
--- frontend/src/components/Feed/IdeaIndexItem.jsx
+++ frontend/src/components/Feed/IdeaIndexItem.jsx
@@ -1,14 +1,36 @@
 import { Link } from "react-router-dom"
 import './IndexItem.css'
-export default function IdeaIndexItem({ idea }) {
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+AOS.init();
+
+export default function IdeaIndexItem({ idea, ideaIndexOrder }) {
+
+    function getAosDelay(ideaIndexOrder) {
+        switch (ideaIndexOrder) {
+            case 'index-item-order1':
+                return '0';
+            case 'index-item-order2':
+                return '150';
+            case 'index-item-order3':
+                return '300';
+            case 'index-item-order4':
+                return '450';
+            default:
+                return '0';
+        }
+    }
+
+
     return (
-        <div className='index-item-wrapper'>
-            <Link to={`/idea/${idea._id}`} className='index-item-link'>
-                <img className='placeholder-box' src={idea.imageUrls[0]}></img>
-
-                <h1 className="index-item-title">{idea.title}</h1>
-
-            </Link>
-        </div>
+        <>
+            <div className={`index-item-wrapper ${ideaIndexOrder}`} data-aos="fade-up" data-aos-duration="1000" data-aos-delay={getAosDelay(ideaIndexOrder)}>
+                <Link to={`/idea/${idea._id}`} className='index-item-link'>
+                    <img className='placeholder-box' src={idea.imageUrls[0]}></img>
+                    <h1 className="index-item-title" >{idea.title}</h1>
+                </Link>
+            </div>
+        </>
     )
 }
