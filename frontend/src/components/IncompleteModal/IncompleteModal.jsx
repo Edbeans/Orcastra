ad338c7fda9632006f66295145cdb82a4a48b075
Daniel Liem <danielliem49@gmail.com>
2023-03-31 14:40:22 -0700

    incomplete modal styling

--- frontend/src/components/IncompleteModal/IncompleteModal.jsx
+++ frontend/src/components/IncompleteModal/IncompleteModal.jsx
@@ -3,6 +3,7 @@ import { useContext } from "react"
 import { IncompleteModalContext } from "../../App";
 import { Modal } from "../../context/modal"
 import './IncompleteModal.css'
+import cryingorca from '../../assets/crying-orca.png'
 
 export default function IncompleteModal() {
 
@@ -12,13 +13,21 @@ export default function IncompleteModal() {
         <>
             {showIncompleteModal && (
                 <Modal onClose={() => setShowIncompleteModal(false)}>
-                    <div className="modal-background" style={{ width: '600px', height: '500px' }}>
-                    <button className="modal-close-button" onClick={() => setShowIncompleteModal(false)}>&#10005;</button>
+                    <div className="modal-background" style={{ width: '550px', height: '700px' }}>
+                        <button className="modal-close-button" onClick={() => setShowIncompleteModal(false)}>&#10005;</button>
                         <div className="incomplete-container">
                             <div className="incomplete-sorry-msg">Blubber!</div>
-                            {/* <img></img> */}
-                            <div className="incomplete-blurb-msg">Our team is working on getting this feature to you as soon as we can.</div>
-                        </div> 
+                            <img src={cryingorca} style={{ width: '400px', height: 'auto' }}></img>
+                            <div className="incomplete-blurb-msg">
+                                <div>
+                                    Sorry about this!
+                                </div>
+                                <br></br>
+                                <div style={{textAlign:'center'}}>
+                                    Our team is working on getting this feature to you as soon as we can.
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </Modal>
             )}
