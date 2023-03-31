8bd688b516360cecdfbac3aaf83dc2eab7b5529b
“Omar <ocamacho812@gmail.com>
2023-03-30 18:02:45 -0700

    Add span element to bid modal to only accept valid inputs

--- frontend/src/components/IdeaBidModal/BidModalForm.jsx
+++ frontend/src/components/IdeaBidModal/BidModalForm.jsx
@@ -11,11 +11,18 @@ export default function BidModalForm({ setOpenBidModal, idea }) {
   const [bidAmount, setBidAmount] = useState(
     bidAmounts.sort((a, b) => b - a)[0]
   );
+  const shown = { opacity: 1 };
+  const hidden = { opacity: 0 };
 
   const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    const maxBidAmount = Math.max(...bidAmounts); // get the maximum bid amount
+    if (bidAmount <= maxBidAmount) {
+      // if the new bid amount is not larger than the current max bid amount, do not create a new bid
+      return;
+    }
     let newBid = {
       bidder,
       idea: idea._id,
@@ -34,7 +41,11 @@ export default function BidModalForm({ setOpenBidModal, idea }) {
           value={bidAmount}
           onChange={(e) => setBidAmount(e.target.value)}
         />
-
+        {bidAmount <= Math.max(...bidAmounts) && (
+          <span style={{ color: 'red' }}>
+            Bid must be greater than ${Math.max(...bidAmounts)}!
+          </span>
+        )}
         <button>Bid!</button>
       </form>
     </div>
