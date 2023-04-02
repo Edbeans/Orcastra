99784ae10f4a417d0f5afcc9cf9c31f7f47679c4
Daniel Liem <danielliem49@gmail.com>
2023-04-02 14:31:20 -0700

    more-footer-styling

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -28,10 +28,21 @@ export default function Footer() {
                 </img>
 
                 <div className="footer-links-container">
-
-                    <button className="meet-the-team-button">
-                        Test
-                    </button>
+                    <div className="footer-mobileapp-container">
+                        <img src="https://cdn-assets.alltrails.com/assets/images/stores/apple-app-store-en-US.svg" className="footer-mobileapp-links" onClick={() => setShowIncompleteModal(true)}>
+                        </img>
+                        <img src="https://cdn-assets.alltrails.com/assets/images/stores/google-play-badge-en-US.svg" className="footer-mobileapp-links" onClick={() => setShowIncompleteModal(true)}>
+                        </img>
+                    </div>
+                    <div>
+                        <a className='footer-contact-link' href="https://github.com/Edbeans/Orcastra" target='_blank'>
+                            Github
+                        </a>
+                        <span style={{ padding: '0px 14px 0px 14px', fontSize: '10px' }}> | </span>
+                        <span className="footer-contact-link" onClick={() => setShowAboutModal(true)}>
+                            Meet the team
+                        </span>
+                    </div>
                 </div>
 
                 <div className="footer-lower-container">
